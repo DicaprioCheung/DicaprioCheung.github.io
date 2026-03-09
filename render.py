8d9b156a9f65c3afb8157772eb9e630fa2d092ff
DicaprioCheung <dhcheungaa@connect.ust.hk>
2026-03-09 08:14:42 +0800

    rename output directory as docs for github page

--- render.py
+++ render.py
@@ -5,27 +5,27 @@ env = Environment(loader=FileSystemLoader('templates'))
 
 # Render pages
 pages = [
-    {'template': 'index.html', 'output': 'output/index.html'},
+    {'template': 'index.html', 'output': 'docs/index.html'},
 
-    {'template': 'education/hkust.html', 'output': 'output/education/hkust.html'},
-    {'template': 'education/plktytc.html', 'output': 'output/education/plktytc.html'},
+    {'template': 'education/hkust.html', 'output': 'docs/education/hkust.html'},
+    {'template': 'education/plktytc.html', 'output': 'docs/education/plktytc.html'},
 
-    {'template': 'experience/10botics.html', 'output': 'output/experience/10botics.html'},
-    {'template': 'experience/hkust-ta.html', 'output': 'output/experience/hkust-ta.html'},
+    {'template': 'experience/10botics.html', 'output': 'docs/experience/10botics.html'},
+    {'template': 'experience/hkust-ta.html', 'output': 'docs/experience/hkust-ta.html'},
 
-    {'template': 'robotics/robocon.html', 'output': 'output/robotics/robocon.html'},
-    {'template': 'robotics/tyt-robotics.html', 'output': 'output/robotics/tyt-robotics.html'},
+    {'template': 'robotics/robocon.html', 'output': 'docs/robotics/robocon.html'},
+    {'template': 'robotics/tyt-robotics.html', 'output': 'docs/robotics/tyt-robotics.html'},
     
-    {'template': 'projects/2v2-basketball.html', 'output': 'output/projects/2v2-basketball.html'},
-    {'template': 'projects/calligraphy-robot.html', 'output': 'output/projects/calligraphy-robot.html'},
-    {'template': 'projects/glyphmaster.html', 'output': 'output/projects/glyphmaster.html'},
-    {'template': 'projects/index.html', 'output': 'output/projects/index.html'},
-    {'template': 'projects/RDC2022_ARC.html', 'output': 'output/projects/RDC2022_ARC.html'},
-    {'template': 'projects/RDC2022_TR.html', 'output': 'output/projects/RDC2022_TR.html'},
-    {'template': 'projects/Robocon2023_trajplanner.html', 'output': 'output/projects/Robocon2023_trajplanner.html'},
-    {'template': 'projects/Robocon2023_RR.html', 'output': 'output/projects/Robocon2023_RR.html'},
-    {'template': 'projects/Robocon2024_R1.html', 'output': 'output/projects/Robocon2024_R1.html'},
-    {'template': 'projects/Robocon2024_R2.html', 'output': 'output/projects/Robocon2024_R2.html'},
+    {'template': 'projects/2v2-basketball.html', 'output': 'docs/projects/2v2-basketball.html'},
+    {'template': 'projects/calligraphy-robot.html', 'output': 'docs/projects/calligraphy-robot.html'},
+    {'template': 'projects/glyphmaster.html', 'output': 'docs/projects/glyphmaster.html'},
+    {'template': 'projects/index.html', 'output': 'docs/projects/index.html'},
+    {'template': 'projects/RDC2022_ARC.html', 'output': 'docs/projects/RDC2022_ARC.html'},
+    {'template': 'projects/RDC2022_TR.html', 'output': 'docs/projects/RDC2022_TR.html'},
+    {'template': 'projects/Robocon2023_trajplanner.html', 'output': 'docs/projects/Robocon2023_trajplanner.html'},
+    {'template': 'projects/Robocon2023_RR.html', 'output': 'docs/projects/Robocon2023_RR.html'},
+    {'template': 'projects/Robocon2024_R1.html', 'output': 'docs/projects/Robocon2024_R1.html'},
+    {'template': 'projects/Robocon2024_R2.html', 'output': 'docs/projects/Robocon2024_R2.html'},
 ]
 
 for page in pages:
